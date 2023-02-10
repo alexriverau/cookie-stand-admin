443ae807b389379ad209e616fa02177b46e88c98
Alejandro Rivera <alexrivera78@gmail.com>
2023-02-09 22:46:29 -0600

    lab37 completed

---
+++ README.md
@@ -0,0 +1,34 @@
+# Lab 37: Welcome to React, Next.js & TailwindCSS
+
+### Project: Cookie Stand Admin
+
+### Author: Alejandro Rivera
+
+---
+
+* Next.js front-end app that allows users to create new cookie-stands
+* Implements Tailwind CSS for styling
+
+### Setup
+
+Must have the latest npm version
+
+* install:
+
+```commandline
+npm install 
+```
+
+* upgrade:
+
+```upgrade
+npm install -g npm@latest
+```
+
+### How to initialize application
+
+```commandline
+npm run dev
+```
+
+Go to: [http://localhost:3000/](http://localhost:3000/)
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 import Head from 'next/head';
+import { useState } from 'react';
 
 export default function Home() {
+  const [cookieStand, createCookieStand] = useState('');
+
+  function createCookieStandHandler(event) {
+    event.preventDefault();
+    const newCookieStand = {
+      'location': event.target.location.value,
+      'minCustomersHour': event.target.minCustomersHour.value,
+      'maxCustomersHour': event.target.maxCustomersHour.value,
+      'avgCookies': event.target.avgCookies.value,
+    }
+    createCookieStand(JSON.stringify(newCookieStand));
+  }
   return (
     <div>
       <Head>
@@ -15,7 +28,7 @@ export default function Home() {
             <h1 className='mb-5 flex justify-center text-4xl'>
               Create Cookie Stand
             </h1>
-            <form>
+            <form onSubmit={createCookieStandHandler}>
               <div className='py-6 flex text-3xl'>
                 <label className='pr-3'>Location</label>
                 <input name='location' className='w-11/12 h-10' />
@@ -23,11 +36,11 @@ export default function Home() {
               <div className='py-6 flex items-center'>
                 <div className='text-3xl text-center'>
                   <label>Minimum Customers per Hour</label>
-                  <input name='minCustHour' className='w-auto h-10' />
+                  <input name='minCustomersHour' className='w-auto h-10' />
                 </div>
                 <div className='p-4 text-3xl text-center'>
                   <label>Maximum Customers per Hour</label>
-                  <input name='maxCustHour' className='w-auto h-10' />
+                  <input name='maxCustomersHour' className='w-auto h-10' />
                 </div>
                 <div className='text-3xl text-center'>
                   <label>Average Cookies per Sale</label>
@@ -41,12 +54,13 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <p className='p-8 mt-6 mb-20 text-2xl text-gray-600 relative flex items-center justify-center'>
+          <p className='p-8 mt-6 text-2xl text-gray-600 relative flex items-center justify-center'>
             Report Table Coming Soon...
           </p>
+          <p className='p-2 text-2xl text-gray-600 relative flex items-center justify-center'>{cookieStand}</p>
         </div>
       </main>
-      <footer className='p-12 bg-green-500'>
+      <footer className='fixed bottom-0 w-full p-10 bg-green-500'>
         <p className='text-3xl text-gray-800'>&copy; 2023</p>
       </footer>
     </div>
